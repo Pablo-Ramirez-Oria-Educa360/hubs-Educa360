7e83a3aa20a98b2cf9d66f3bcc5341f83ac61e2d
Pablo-Ramirez-Oria <pabloramirezoria@gmail.com>
2026-02-11 12:40:43 +0100

    Fixed bug that removed ownership of a scene

--- src/utils/take-ownership.ts
+++ src/utils/take-ownership.ts
@@ -4,7 +4,16 @@ import { AEntity, Networked, Owned } from "../bit-components";
 import { getServerTime } from "../phoenix-adapter";
 import type { EntityID } from "./networking-types";
 
+function isValidEntityId(eid: EntityID | undefined): eid is EntityID {
+  return typeof eid === "number" && Number.isFinite(eid);
+}
+
 export function takeOwnership(world: HubsWorld, eid: EntityID) {
+  if (!isValidEntityId(eid)) {
+    console.warn("takeOwnership called with invalid entity id:", eid);
+    return;
+  }
+
   // TODO we do this to have a single API for taking ownership of things in new code, but it obviously relies on NAF/AFrame
   if (hasComponent(world, AEntity, eid)) {
     const el = world.eid2obj.get(eid)!.el!;
--- src/utils/take-soft-ownership.ts
+++ src/utils/take-soft-ownership.ts
@@ -3,7 +3,16 @@ import { HubsWorld } from "../app";
 import { AEntity, Networked, Owned } from "../bit-components";
 import type { EntityID } from "./networking-types";
 
+function isValidEntityId(eid: EntityID | undefined): eid is EntityID {
+  return typeof eid === "number" && Number.isFinite(eid);
+}
+
 export function takeSoftOwnership(world: HubsWorld, eid: EntityID) {
+  if (!isValidEntityId(eid)) {
+    console.warn("takeSoftOwnership called with invalid entity id:", eid);
+    return;
+  }
+
   if (hasComponent(world, AEntity, eid)) {
     throw new Error("Cannot take soft ownership of AEntities.");
   }
